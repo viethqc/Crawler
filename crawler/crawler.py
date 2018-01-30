6d23655628ea7054e47b5f6219e403c4ba0257e9
Hoàng Quốc Việt <hoangvietbk2.06@gmail.com>
2018-01-30 22:43:35 +0700

    create function need to craw

---
+++ crawler/crawler.py
@@ -0,0 +1,11 @@
+class Crawler :
+    def __init__(self):
+        print "hello"
+    def setUrl(self, url):
+        return ""
+    def setPathStorage(self, url):
+        return ""
+
+    def startCrawler():
+        return ""
+    
