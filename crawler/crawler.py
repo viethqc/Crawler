dc841037ff02c8f4547fcc63c0d0f6434f044f74
Hoàng Quốc Việt <hoangvietbk2.06@gmail.com>
2018-02-03 00:06:08 +0700

    done crawler

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,4 +1,8 @@
 import os
+import requests
+from pyquery import PyQuery
+import urllib
+
 try:
     from urllib.parse import urlparse
 except ImportError:
@@ -6,25 +10,62 @@ except ImportError:
 
 class Crawler :
     def __init__(self):
-        self.url = ""
-        self.serverName = ""
-        self.objectName = ""
+        self.__url = ""
+        self.__schema = ""
+        self.__serverName = ""
+        self.__objectName = ""
+        self.__pathStorage = ""
 
     def setUrl(self, url):
-        self.url = url
+        self.__url = url
         parser = urlparse(url)
 
-        self.serverName = parser.netloc
-        self.objectName = parser.path
-
-        print self.serverName, self.objectName
+        self.__serverName = parser.netloc
+        self.__objectName = parser.path
+        self.__schema = parser.scheme
 
     def setPathStorage(self, pathStorage):
         if (os.path.exists(pathStorage) == False) :
             os.makedirs(pathStorage)
 
+        self.__pathStorage = pathStorage
         return True
 
     def startCrawler(self):
+        if self.__url == "" :
+            return False
+
+        request = requests.get(self.__url)
+        pyquery = PyQuery(request.content)
+        listChap = pyquery(".ChI a")
+        for i in range(listChap.length) :
+            chap = listChap.eq(i)
+            self.__singleCrawler(self.__schema + "://" + self.__serverName + chap.attr["href"], os.path.join(self.__pathStorage, chap.text()))
         return ""
+
+    def __singleCrawler(self, link, folder):
+        fileDone = os.path.join(folder, "done")
+        linkImg = ""
+        fileName = ""
+
+        if os.path.exists(folder) == False :
+            os.makedirs(folder)
+
+        response = requests.get(link)
+        if (response.status_code != 200) : 
+            return False
+
+        if os.path.exists(fileDone) == True :
+            return True
+
+        pyquery = PyQuery(response.content)
+        listImg = pyquery("#ctl14_PC img")
+        for i in range(listImg.length) :
+            img = listImg.eq(i)
+            linkImg = img.attr["src"]
+            fileName = os.path.join(folder, str(i) + ".jpg")
+            urllib.urlretrieve(linkImg, fileName)
+
+        f= open(fileDone,"w+")
+        return True
     
